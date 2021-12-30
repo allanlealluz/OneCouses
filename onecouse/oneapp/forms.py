dfcf93f71ad8a5ece8c499da7ea9a8aa09395fac
allanlealluz <allanlealluz@gmail.com>
2021-12-30 18:50:31 -0300

    simple update

--- onecouse/oneapp/forms.py
+++ onecouse/oneapp/forms.py
@@ -1,5 +1,5 @@
 from django.forms import forms
 from django import forms
 class registerForm(forms.Form):
-    name = forms.CharField(label='Your name')
-    password = forms.PasswordInput()
+    name = forms.CharField(label='Your name',widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput())
